9d040c5f4f555346857b307b83527c03000c246c
Maraat <maraatto@gmail.com>
2016-05-18 22:36:42 +0200

    Changed to firebase google Now you can set a custom name

--- app/pages/add/add.ts
+++ app/pages/add/add.ts
@@ -6,23 +6,30 @@ import {Page, NavController} from 'ionic-angular';
 export class Add {
     server: any;
     nav: NavController;
-    auth: any;
     users: Array<any>;
     player2: any;
+    firebase: any;
+    user: string;
 
     constructor(nav: NavController) {
         this.nav = nav;
         this.users = [];
-
-        var Firebase = require("firebase");
-        this.server = new Firebase('https://blazing-heat-2153.firebaseio.com/');
-        this.auth = this.server.getAuth();
 
         this.initializeApp();
     }
 
     initializeApp() {
         var parent = this;
+        var config = {
+          apiKey: "AIzaSyDTdCQ96RtfLa39NKgmwpwhnjrVKdJEKtw",
+          authDomain: "blazing-heat-2153.firebaseapp.com",
+          databaseURL: "https://blazing-heat-2153.firebaseio.com",
+          storageBucket: "blazing-heat-2153.appspot.com",
+        };
+        this.firebase = require("firebase");
+        this.server = firebase.database().ref();
+        this.user = firebase.auth().currentUser.displayName;
+
         this.server.child('users').on('child_added', function (snapshot) {
             var message = snapshot.val();
             parent.users.push(message.name);
@@ -33,7 +40,7 @@ export class Add {
 
     addGame(p1score, p2score) {
         this.server.child('games').push({
-            player1: this.auth.google.displayName,
+            player1: this.user,
             player2: this.player2,
             p1Score: p1score,
             p2Score: p2score
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import {Page, Platform} from 'ionic-angular';
+import {Page, Platform, Alert, NavController} from 'ionic-angular';
 import {Add} from '../add/add';
 
 @Page({
@@ -11,20 +11,29 @@ export class Home {
     platform: Platform;
     auth: any;
     server: any;
+    firebase: any;
+    nav: any;
 
-    constructor(platform: Platform) {
+    constructor(platform: Platform, nav: NavController) {
         this.games = [];
+        this.nav = nav;
         this.addPage = Add;
         this.platform = platform;
-        this.initializeApp();    
+        this.initializeApp();
     }
 
     initializeApp() {
         this.platform.ready().then(() => {
             var parent = this;
-            var Firebase = require("firebase");
-            this.server = new Firebase('https://blazing-heat-2153.firebaseio.com/');
-
+            var config = {
+              apiKey: "AIzaSyDTdCQ96RtfLa39NKgmwpwhnjrVKdJEKtw",
+              authDomain: "blazing-heat-2153.firebaseapp.com",
+              databaseURL: "https://blazing-heat-2153.firebaseio.com",
+              storageBucket: "blazing-heat-2153.appspot.com",
+            };
+            this.firebase = require("firebase");
+            firebase.initializeApp(config);
+            this.server = firebase.database().ref();
             this.server.child('games').on('child_added', function (snapshot) {
                 var message = snapshot.val();
                 parent.games.push({
@@ -34,27 +43,49 @@ export class Home {
                     p2Score: message.p2Score
                 });
             });
-            this.auth = this.server.getAuth();
+
+            firebase.auth().onAuthStateChanged(function(user) {
+              if (user) {
+                parent.auth = true;
+              } else {
+                parent.auth = false;
+              }
+            });
         });
     }
 
     login() {
-        if (this.auth) {
-            console.log("User " + this.auth.uid + " is logged in with " + this.auth.provider);
-        } else {
-            var vm = this;
-            this.server.authWithOAuthPopup("google", function (error, authData) {
-                if (error) {
-                    console.log("Login Failed!", error);
-                } else {
-                    console.log("Authenticated successfully with payload:", authData);
-                    vm.auth = authData;
-                    vm.server.child('users/' + authData.uid).set({
-                        name: authData.google.displayName
-                    });
-                }
-            });
-        }
+      var vm = this;
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+        console.log(result);
+        let prompt = Alert.create({
+          title: "Introduce tu nombre",
+          inputs: [
+            {
+              name: 'name',
+              placeholder: 'Nombre'
+            },
+          ],
+          buttons: [
+            {
+              text: 'Save',
+              handler: data => {
+                var user = firebase.auth().currentUser;
+                user.updateProfile({
+                  displayName: data.name
+                });
+                vm.server.child('users/' + result.user.uid).set({
+                  name: data.name
+                });
+              }
+            }
+          ]
+        });
+        vm.nav.present(prompt);
+      }).catch(function(error) {
+        console.log("Login Failed!", error);
+      });
     }
 }
 
